e06973a5a736bf518f2e2f658b116da1a00fc1b9
jbsk1 <104356522+jbsk1@users.noreply.github.com>
2022-12-05 11:45:21 +0100

    Update Variables.cs
    Fixed

--- Assets/Script/Juraj/Variables.cs
+++ Assets/Script/Juraj/Variables.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    static Variables instance;
+    public static Variables instance;
     public int money = 0;
     public int oil = 0;
     public int oilperclick = 1;
 
-    private void Awake()
+    void Awake()
     {
         instance = this;
     }
