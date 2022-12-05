9321d2d16438b17f1631002f924e560d58336d1d
jbsk1 <104356522+jbsk1@users.noreply.github.com>
2022-12-05 14:34:45 +0100

    Script updates

--- Assets/Script/Juraj/ClickObj.cs
+++ Assets/Script/Juraj/ClickObj.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClickObj : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]GameObject particle;
+    TMP_Text text;
+
+    void Awake()
     {
-        
+        text = particle.GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick()
     {
-        
+        var vars = Variables.instance;
+
+        vars.oil += vars.oilperclick;
+        SpawnParticle();
+    }
+
+    void SpawnParticle()
+    {
+        var vars = Variables.instance;
+
+        text.text = vars.oilperclick.ToString(); //makes particle show you how much you earn
+        //text.fontSize = Random.Range(1,1);
+        StartCoroutine(_Particle());
+    }
+
+    IEnumerator _Particle()
+    {
+        GameObject particle = Instantiate(this.particle, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+        //int direction = Random.Range(0,360);
+        float speed = 1;
+        int lifetime = 500;
+        for(int i = 0; i < lifetime; i++)
+        {
+            particle.transform.position += new Vector3(0, speed*Time.deltaTime, 0);
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(particle);
     }
 }
