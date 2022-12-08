53235bee151817d6bfccabcaf5c032a8a7750ef8
jbsk1 <104356522+jbsk1@users.noreply.github.com>
2022-12-08 08:45:03 +0100

    Update Text Particles
    Updated transition + renamed variables

--- Assets/Script/Juraj/ClickObj.cs
+++ Assets/Script/Juraj/ClickObj.cs
@@ -6,11 +6,13 @@ using TMPro;
 public class ClickObj : MonoBehaviour
 {
     [SerializeField]GameObject particle;
-    TMP_Text text;
+    TMP_Text pText;
+    float pSpeed = 1;
+    int pFrameTime = 500;
 
     void Awake()
     {
-        text = particle.GetComponent<TMP_Text>();
+        pText = particle.GetComponent<TMP_Text>();
     }
 
     public void OnClick()
@@ -24,21 +26,21 @@ public class ClickObj : MonoBehaviour
     void SpawnParticle()
     {
         var vars = Variables.instance;
-
-        text.text = vars.oilperclick.ToString(); //makes particle show you how much you earn
-        //text.fontSize = Random.Range(1,1);
+        pText.text = vars.oilperclick.ToString(); //makes particle show you how much you earn
+        
         StartCoroutine(_Particle());
     }
 
     IEnumerator _Particle()
     {
         GameObject particle = Instantiate(this.particle, gameObject.transform.position, Quaternion.identity, gameObject.transform);
-        //int direction = Random.Range(0,360);
-        float speed = 1;
-        int lifetime = 500;
-        for(int i = 0; i < lifetime; i++)
+        TMP_Text pText = particle.GetComponent<TMP_Text>();
+        
+        for(int i = 0; i < pFrameTime; i++)
         {
-            particle.transform.position += new Vector3(0, speed*Time.deltaTime, 0);
+            particle.transform.position += new Vector3(0, pSpeed*Time.deltaTime, 0);
+            pText.color -= new Color(0,0,0, 1f/pFrameTime);
+            //Debug.Log(pText.color.a);
             yield return new WaitForEndOfFrame();
         }
         Destroy(particle);
